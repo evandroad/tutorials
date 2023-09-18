cf30f5c45c99ac6bb41c4f48743e202e2f3f9110
evandro.abreu <evandro.abreu@digicon.com.br>
2023-09-18 16:53:36 -0300

    number in commands

--- main/main.js
+++ main/main.js
@@ -6,7 +6,7 @@ const contentBody = document.getElementById('contentBody')
 window.onload = () => {
     contentContainer.style.display = 'none'
 
-    fetch("main/tutorials.json")
+    fetch("data/tutorials.json")
         .then(response => response.json())
         .then(json => {
             var menu = document.getElementById('menuTutorials')
@@ -38,70 +38,72 @@ function getPage(page) {
     fetch("data/" + page + ".json")
         .then(response => response.json())
         .then(json => {
-            // SUMMARY
-
-            const sidenav = document.createElement('div')
-            sidenav.setAttribute('class', 'sidenav')
-
-            const divHome = document.createElement('div')
-            divHome.setAttribute('id','divHome');
-            
-            const linkHome = document.createElement('a')
-            linkHome.setAttribute('href', 'index.html')
-
-            const logoHome = document.createElement('i')
-            logoHome.setAttribute('class', 'fa fa-home')
-            logoHome.setAttribute('id', 'logoHome')
-            logoHome.setAttribute('style', 'font-size: 50px')
-
-            const summary = document.createElement('h2')
-            summary.setAttribute('id', 'summary')
-            summary.innerHTML = 'Summary'
-            
-            linkHome.appendChild(logoHome)
-            divHome.appendChild(linkHome)
-            sidenav.appendChild(divHome)
-            sidenav.appendChild(summary)
-            
-            Object.keys(json).forEach(ele => {
-                const li = document.createElement('li')
-                li.setAttribute('class', 'summaryItem')
-
-                const link = document.createElement('a')
-                link.setAttribute('href', '#' + ele)
-                link.setAttribute('class', 'linkMenu')
-                link.innerHTML = ele
-                
-                li.appendChild(link)
-                sidenav.appendChild(li)
-            })
-
-            contentMenu.appendChild(sidenav)
-
-            // CONTENT
-
-            const title = document.createElement('h1')
-            title.innerHTML = page
-            contentBody.appendChild(title)
-            
-            for(i = 0; i < Object.keys(json).length; i++) {
-                var item = Object.keys(json)[i]
-                
-                const title = document.createElement('h2')
-                title.setAttribute('id', item)
-                title.innerHTML = item
-                contentBody.appendChild(title)
-
-                if(json[item].length > 1) {
-                    json[item].forEach(ele => {
-                        createContentAndCode(ele, contentBody)
-                    });
-                } else {
-                    createContentAndCode(json[item][0], contentBody)
-                }
-            }
+            json.sort(compareByNumber);
+            renderMenu(json, page)
+            renderBody(json, page)
         })
         .catch(err => console.log('Error in the request', err))
+}
+
+function renderMenu(data) {
+    const sidenav = document.createElement('div')
+    sidenav.setAttribute('class', 'sidenav')
+
+    const divHome = document.createElement('div')
+    divHome.setAttribute('id','divHome');
+    
+    const linkHome = document.createElement('a')
+    linkHome.setAttribute('href', 'index.html')
+
+    const logoHome = document.createElement('i')
+    logoHome.setAttribute('class', 'fa fa-home')
+    logoHome.setAttribute('id', 'logoHome')
+    logoHome.setAttribute('style', 'font-size: 50px')
+
+    const summary = document.createElement('h2')
+    summary.setAttribute('id', 'summary')
+    summary.innerHTML = 'Summary'
+    
+    linkHome.appendChild(logoHome)
+    divHome.appendChild(linkHome)
+    sidenav.appendChild(divHome)
+    sidenav.appendChild(summary)
+    
+    data.forEach(item => {
+        const li = document.createElement('li')
+        li.setAttribute('class', 'summaryItem')
+        
+        const link = document.createElement('a')
+        link.setAttribute('href', '#' + item.title)
+        link.setAttribute('class', 'linkMenu')
+        link.innerHTML = item.number + ' - ' + item.title
+        
+        li.appendChild(link)
+        sidenav.appendChild(li)
+    })
+    
+    contentMenu.appendChild(sidenav)
+}
+
+function renderBody(data, page) {
+    const title = document.createElement('h1')
+    title.innerHTML = page
+    contentBody.appendChild(title)
+    
+    data.forEach(item => {
+        const title = document.createElement('h2')
+        title.setAttribute('id', item.title)
+        title.innerHTML = item.number + ' - ' + item.title
+        contentBody.appendChild(title)
+        
+        item.content.forEach(ele => {
+            createContentAndCode(ele, contentBody)
+        });
+    })
+}
+
+function compareByNumber(a, b) {
+    return a.number - b.number;
 }
 
 function createContentAndCode(obj, main) {
@@ -121,14 +123,15 @@ function createContentAndCode(obj, main) {
 
 btnTop = document.getElementById("btnTop");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         btnTop.style.display = "block";
-    } else {
-        btnTop.style.display = "none";
+        return
     }
+
+    btnTop.style.display = "none";
 }
 
 function topFunction() {
