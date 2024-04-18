0c1c6413071b51041acda28c9da4c69e5fc9dd6d
Evandro A. Domingues <evandro.domingues@hotmail.com>
2024-04-17 23:59:13 -0300

    test go

---
+++ generate-json/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"encoding/json"
+	"io/ioutil"
+	"io"
+	"strconv"
+)
+
+type Data struct {
+	Message string `json:"message"`
+}
+
+type Tutorial struct {
+	Number int    `json:"number"`
+	Title  string `json:"title"`
+	Image  string `json:"image"`
+}
+
+type TutorialList struct {
+	Tutorials []Tutorial `json:"tutorials"`
+}
+
+func (tl *TutorialList) LoadFromFile(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, tl)
+}
+
+func (tl *TutorialList) AddTutorial(tutorial Tutorial) {
+	tl.Tutorials = append(tl.Tutorials, tutorial)
+}
+
+func (tl *TutorialList) ToJson() ([]byte, error) {
+	return json.MarshalIndent(tl, "", "  ")
+}
+
+func main() {
+	staticDir := "./"
+
+	http.Handle("/", http.FileServer(http.Dir(staticDir)))
+	http.HandleFunc("/api/data", handleData)
+	http.HandleFunc("/api/insert", insertHandler)
+
+	port := ":8080"
+	println("Servidor rodando em http://localhost" + port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		println("Erro ao iniciar o servidor: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	data := Data{Message: "Hello from Go server!"}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Erro ao codificar JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func insertHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20) // 10 MB max memory
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Read the form values
+	number, err := strconv.Atoi(r.FormValue("number"))
+	if err != nil {
+		http.Error(w, "Invalid or missing number", http.StatusBadRequest)
+		return
+	}
+	title := r.FormValue("tutorial")
+	if title == "" {
+		http.Error(w, "Invalid or missing tutorial title", http.StatusBadRequest)
+		return
+	}
+
+	// Read the file
+	file, handler, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Invalid or missing image file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Create file
+	dst, err := os.Create("../" + handler.Filename)
+	if err != nil {
+		http.Error(w, "Failed to create file", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	// Copy the file to the destination
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, "Failed to save file", http.StatusInternalServerError)
+		return
+	}
+
+	tutorial := Tutorial{Number: int(number), Title: title, Image: handler.Filename}
+	
+	path := "tutorials.json"
+	tutorialList := &TutorialList{}
+	err = tutorialList.LoadFromFile(path)
+	if err != nil {
+		http.Error(w, "Failed to load tutorials", http.StatusInternalServerError)
+		return
+	}
+
+	tutorialList.AddTutorial(tutorial)
+
+	jsonData, err := tutorialList.ToJson()
+	if err != nil {
+		http.Error(w, "Failed to serialize tutorials", http.StatusInternalServerError)
+		return
+	}
+
+	ioutil.WriteFile(path, jsonData, 0644)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Created tutorial."})
+}
--- generate-json/php/controllers/TutorialController.php
+++ generate-json/php/controllers/TutorialController.php
@@ -6,125 +6,125 @@ include 'models/TutorialList.php';
 
 class TutorialController {
 
-    function getAll() {
-        $path = '../../tutorials/data/tutorials.json';
+  function getAll() {
+    $path = '../../tutorials/data/tutorials.json';
 
-        $tutorialList = new TutorialList();
-        $tutorialList->loadFromFile($path);
+    $tutorialList = new TutorialList();
+    $tutorialList->loadFromFile($path);
 
-        echo $tutorialList->toJson();
-    }
+    echo $tutorialList->toJson();
+  }
 
-    function insert() {
-        $req = getBody();
-        
-        $number = $req['number'];
-        $title = $req['tutorial'];
-        $image = $req['image'];
-
-        if ($this->createMoveFoldersFiles($title, $image)) {
-            return;
-        }
-
-        $path = '../../tutorials/data/tutorials.json';
-
-        $tutorialList = new TutorialList();
-        $tutorialList->loadFromFile($path);
-
-        $tutorial = new Tutorial(intval($number), $title, $image['name']);
-        $tutorialList->addTutorial($tutorial);
-
-        file_put_contents($path, $tutorialList->toJson());
-        
-        echo json_encode(["message" => "Created tutorial."]);
-    }
-
-    function update() {
-        $req = getBody();
-
-        $number = $req['number'] ?? 0;
-        $currentTitle = $req['currentTutorial'] ?? '';
-        $title = $req['tutorial'] ?? '';
-        $currentImage = $req['currentImage'] ?? '';
-        $image = $req['image'] ?? '';
-        
-        $this->updateMoveFiles($currentImage, $image, $currentTitle, $title);
-
-        $path = '../../tutorials/data/tutorials.json';
-
-        $tutorialList = new TutorialList();
-        $tutorialList->loadFromFile($path);
-
-        $tutorial = new Tutorial(intval($number), $title, $image['name'] ?? '');
-        $tutorialList->updateTutorial($currentTitle, $tutorial);
-        
-        file_put_contents($path, $tutorialList->toJson());
-
-        echo json_encode(["message" => "Updated tutorial."]);
-    }
-
-    function delete($params) {
-        $tutorial = $params["name"];
+  function insert() {
+    $req = getBody();
     
-        unlink("../../tutorials/data/".$tutorial.".json");
-        
-        $path = "../../tutorials/data/tutorials.json";
+    $number = $req['number'];
+    $title = $req['tutorial'];
+    $image = $req['image'];
 
-        $tutorialList = new TutorialList();
-        $tutorialList->loadFromFile($path);
-
-        $tutorialList->deleteTutorial($tutorial);
-        
-        file_put_contents($path, $tutorialList->toJson());
-
-        echo json_encode(["message" => "Tutorial deletado."]);
+    if ($this->createMoveFoldersFiles($title, $image)) {
+      return;
     }
 
-    function createMoveFoldersFiles($title, $image) {
-        $dirData = "../../tutorials/data";
-        $dirImg = "../../tutorials/img";
+    $path = '../../tutorials/data/tutorials.json';
 
-        $pathTutorial = $dirData.'/'.$title.'.json';
-        $pathImage = $dirImg.'/'.$image['name'];
-        $pathTutorials = $dirData.'/tutorials.json';
+    $tutorialList = new TutorialList();
+    $tutorialList->loadFromFile($path);
 
-        if (!file_exists($dirData)) {
-            mkdir($dirData, 777, true);
-            file_put_contents($pathTutorials, '[]');
-        }
-        
-        if (!file_exists($dirImg)) mkdir($dirImg, 777, true);
-                
-        if (file_exists($pathTutorial)) {
-            echo json_encode(["message" => "Tutorial already exists."]);
-            return true;
-        }
+    $tutorial = new Tutorial(intval($number), $title, $image['name']);
+    $tutorialList->addTutorial($tutorial);
 
-        file_put_contents($pathTutorial, '[]');
-        move_uploaded_file($image['tmp_name'], $pathImage);
-        return false;
+    file_put_contents($path, $tutorialList->toJson());
+    
+    echo json_encode(["message" => "Created tutorial."]);
+  }
+
+  function update() {
+    $req = getBody();
+
+    $number = $req['number'] ?? 0;
+    $currentTitle = $req['currentTutorial'] ?? '';
+    $title = $req['tutorial'] ?? '';
+    $currentImage = $req['currentImage'] ?? '';
+    $image = $req['image'] ?? '';
+    
+    $this->updateMoveFiles($currentImage, $image, $currentTitle, $title);
+
+    $path = '../../tutorials/data/tutorials.json';
+
+    $tutorialList = new TutorialList();
+    $tutorialList->loadFromFile($path);
+
+    $tutorial = new Tutorial(intval($number), $title, $image['name'] ?? '');
+    $tutorialList->updateTutorial($currentTitle, $tutorial);
+    
+    file_put_contents($path, $tutorialList->toJson());
+
+    echo json_encode(["message" => "Updated tutorial."]);
+  }
+
+  function delete($params) {
+    $tutorial = $params["name"];
+
+    unlink("../../tutorials/data/".$tutorial.".json");
+    
+    $path = "../../tutorials/data/tutorials.json";
+
+    $tutorialList = new TutorialList();
+    $tutorialList->loadFromFile($path);
+
+    $tutorialList->deleteTutorial($tutorial);
+    
+    file_put_contents($path, $tutorialList->toJson());
+
+    echo json_encode(["message" => "Tutorial deletado."]);
+  }
+
+  function createMoveFoldersFiles($title, $image) {
+    $dirData = "../../tutorials/data";
+    $dirImg = "../../tutorials/img";
+
+    $pathTutorial = $dirData.'/'.$title.'.json';
+    $pathImage = $dirImg.'/'.$image['name'];
+    $pathTutorials = $dirData.'/tutorials.json';
+
+    if (!file_exists($dirData)) {
+      mkdir($dirData, 777, true);
+      file_put_contents($pathTutorials, '[]');
+    }
+    
+    if (!file_exists($dirImg)) mkdir($dirImg, 777, true);
+            
+    if (file_exists($pathTutorial)) {
+      echo json_encode(["message" => "Tutorial already exists."]);
+      return true;
     }
 
-    function updateMoveFiles($currentImage, $image, $currentTitle, $title) {
-        $dirImg = "../../tutorials/img/";
-        $dirData = "../../tutorials/data/";
-        $currentFile = $dirData.$currentTitle.".json";
-        $file = $dirData.$title.".json";
- 
-        rename($currentFile, $file);
+    file_put_contents($pathTutorial, '[]');
+    move_uploaded_file($image['tmp_name'], $pathImage);
+    return false;
+  }
 
-        if (empty($image)) {
-            return;
-        }
-        
-        $pathImage = $dirImg.$image['name'];
-        $pathCurrentImage = $dirImg.$currentImage;
+  function updateMoveFiles($currentImage, $image, $currentTitle, $title) {
+    $dirImg = "../../tutorials/img/";
+    $dirData = "../../tutorials/data/";
+    $currentFile = $dirData.$currentTitle.".json";
+    $file = $dirData.$title.".json";
 
-        if (is_file($pathCurrentImage)) {
-            unlink($pathCurrentImage);
-        }
-        
-        move_uploaded_file($image['tmp_name'], $pathImage);
+    rename($currentFile, $file);
+
+    if (empty($image)) {
+      return;
     }
+    
+    $pathImage = $dirImg.$image['name'];
+    $pathCurrentImage = $dirImg.$currentImage;
+
+    if (is_file($pathCurrentImage)) {
+      unlink($pathCurrentImage);
+    }
+    
+    move_uploaded_file($image['tmp_name'], $pathImage);
+  }
     
 }
